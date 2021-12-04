8129fdd73ba9e695f918218b96d18ebbf0207427
prabesht80 <prabesht80@gmail.com>
2021-12-04 11:16:26 +0545

    login

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,7 +1,76 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <>
+      <div className="containe mx-auto">
+        <div className="flex justify-center py-24">
+          <div className="w-2/5 md:w-1/5 bg-gray-50 py-4 px-8 rounded">
+            <div>
+              <h1 className="italic text-4xl text-center font-bold text-gray-700">
+                Crypto
+              </h1>
+              <form className=" mt-8">
+                <div className="flex flex-col ">
+                  <div className="flex flex-col ">
+                    <label
+                      className="text-gray-700 text-medium font-semibold "
+                      for="userName"
+                    >
+                      username
+                    </label>
+                    <input
+                      className="p-2 rounded-lg shadow border focus:outline-none focus:shadow-out focus:ring-2 focus:ring-gray-400"
+                      id="userName"
+                      type="text"
+                      placeholder="UserName.."
+                    />
+                  </div>
+                  <div className="flex flex-col mt-2 ">
+                    <label
+                      className="text-gray-700 text-medium font-semibold "
+                      for="password"
+                    >
+                      Password
+                    </label>
+                    <input
+                      className="p-2 rounded-lg shadow border focus:outline-none focus:shadow-out focus:ring-2 focus:ring-gray-400"
+                      id="password"
+                      type="password"
+                      placeholder="******"
+                    />
+                  </div>
+                  <div class="mt-6 text-center">
+                    <button
+                      class="w-full px-4 py-2 font-bold text-white bg-blue-500 rounded-full hover:bg-blue-700 focus:outline-none focus:shadow-outline"
+                      type="button"
+                    >
+                      <Link to="/homepage">Login</Link>
+                    </button>
+                  </div>
+                  <div className=" flex justify-between mt-2 ">
+                    <div className="w-32 h-0.5 border mt-3 bg-gray-100"></div>
+                    <div>OR</div>
+                    <div className="w-32 h-0.5 border mt-3 bg-gray-400"></div>
+                  </div>
+                  <p className="mb-2 text-center text-sm font-medium text-gray-700 hover:text-black cursor-pointer">
+                    Forget password?
+                  </p>
+                  <p className="mb-4 text-center text-sm font-medium text-gray-700 hover:text-black cursor-pointer">
+                    Don't have an Account?{' '}
+                    <span className="text-blue-700 hover:text-blue-500">
+                      <Link to="/"> sign up</Link>
+                    </span>
+                  </p>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Login;
--- src/views/Register.js
+++ src/views/Register.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
   return (
@@ -11,11 +12,11 @@ const Register = () => {
             <div
               className="w-full h-auto bg-gray-400 hidden lg:block lg:w-5/12 bg-cover rounded-l-lg"
               style={{
-                backgroundImage: `url('https://source.unsplash.com/Mv9hjnEUHR4/600x800')`,
+                backgroundImage: `url('./images/prabeshDada.jpg')`,
               }}
             ></div>
             {/* column */}
-            <div className="w-full lg:w-7/12 bg-white p-5 rounded-lg lg:rounded-l-none">
+            <div className="w-full lg:w-7/12 bg-gray-50 p-5 rounded-lg lg:rounded-l-none">
               <h2 className="text-2xl text-center">Create an Account</h2>
               <form className=" px-2 pt-8 pb-4 mb-4 bg-white-100 ">
                 <div className="md:flex md:justify-between">
@@ -105,7 +106,7 @@ const Register = () => {
                     class="w-full px-4 py-2 font-bold text-white bg-blue-500 rounded-full hover:bg-blue-700 focus:outline-none focus:shadow-outline"
                     type="button"
                   >
-                    Register Account
+                    <Link to="/login">Register Account</Link>
                   </button>
                 </div>
 
@@ -114,10 +115,15 @@ const Register = () => {
                   Forget password?
                 </p>
                 <p className="text-center text-sm font-medium text-gray-700 hover:text-black cursor-pointer">
-                  Aready ave an Account..
+                  <Link to="/login">Aready have an Account..</Link>
                 </p>
               </form>
             </div>
+          </div>
+          <div className=" flex justify-center items-center mt-96">
+            <button className="p-2 text-gray-700 text-center font-bold rounded-lg bg-gray-300 hover:bg-yellow-300 focus:ring-2 focus:ring-yellow-200 ring-offset-1">
+              <Link to="/homepage">Skip...</Link>
+            </button>
           </div>
         </div>
       </div>
